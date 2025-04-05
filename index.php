036ae1492036379204887dda227cb9cae9313cb7
gugu <psdevco@gmail.com>
2025-04-05 16:55:25 -0400

    initial

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$conn = mysqli_connect('localhost', 'ps', 'ps', 'social');
+if (mysqli_connect_errno()) {
+    echo 'Failed to connect-------------: ' . mysqli_connect_errno();
+}
+
+$query = mysqli_query($conn, "INSERT INTO test  VALUES(NULL,'kaka')");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>facebook clone</title>
+</head>`
+
+<body>
+    Hello
+</body>
+
+</html>
