1681e433ee522c00fff875a9246950f259dbdced
gugu <psdevco@gmail.com>
2025-04-18 01:31:22 -0400

    login form functionality creation

---
+++ includes/form_handlers/login_handler.php
@@ -0,0 +1,31 @@
+
+<?php 
+
+if(isset($_POST['login_btn'])){
+
+	$email = filter_var($_POST['login_email'],FILTER_SANITIZE_EMAIL);//sanitize. email
+
+	$_SESSION['login_email'] = $email;
+
+	$password = md5($_POST['login-password']);
+
+
+	$check_db_query = mysqli_query($conn,"SELECT * FROM users WHERE email='$email' AND password ='$password'");
+	$result_rows= mysqli_num_rows($check_db_query);
+
+	if($result_rows==1){
+
+		//access results from query and store it in $row
+		$row = mysqli_fetch_array($check_db_query); 
+
+		$username = $row['username'];
+
+		$_SESSION['username'] = $username;
+
+		header("Location:index.php");
+
+		exit();
+	}
+}
+
+ ?>
