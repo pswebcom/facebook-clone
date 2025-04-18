3af2c985061b798083b2564cc3b34005925ed796
gugu <psdevco@gmail.com>
2025-04-18 10:49:10 -0400

    login_handler file__log user in if logged out

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -20,12 +20,18 @@ if(isset($_POST['login_btn'])){
 		//access results from query and store it in $row
 		$row = mysqli_fetch_array($check_db_query); 
 
-
 		$username = $row['username'];
-
-		
-
 		$_SESSION['username'] = $username;
+
+
+        //check if user is logged out
+		$user_account_closed_query = mysqli_query($conn,"SELECT * FROM users WHERE email='$email' AND user_closed='yes'");
+		if(mysqli_num_rows($user_account_closed_query) == 1){
+			//sign in user
+          $reopen_account = mysqli_query($conn,"UPDATE users SET user_closed='no' WHERE email='$email'");
+		}
+
+
 
 		header("Location:index.php");
 
